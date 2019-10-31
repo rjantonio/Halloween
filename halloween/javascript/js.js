c17da0aa7cbd3ccabf5d223966a5b76b90f14678
rjantonio <55783021+rjantonio@users.noreply.github.com>
2019-10-31 20:15:23 +0100

    Add files via upload

---
+++ halloween/javascript/js.js
@@ -0,0 +1,14 @@
+<script>
+        function mostrar(elemento) {
+            document.getElementById(elemento).style.visibility="visible";
+        }
+        function ocultar(elemento) {
+            document.getElementById(elemento).style.visibility="hidden";
+        }
+        function sonar(sonido){
+            document.getElementById(sonido).play();
+        }
+        function sonar(sonido){
+            document.getElementById(sonido).pause();
+        }
+        </script>
---
+++ halloween/js.js
@@ -0,0 +1,13 @@
+
+        function mostrar(elemento) {
+            document.getElementById(elemento).style.visibility="visible";
+        }
+        function ocultar(elemento) {
+            document.getElementById(elemento).style.visibility="hidden";
+        }
+        function sonar(sonido){
+            document.getElementById(sonido).play();
+        }
+        function sonar(sonido){
+            document.getElementById(sonido).pause();
+        }
